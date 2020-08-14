a7ae26cfa11773925fb867ad1451fe53e4f6d1f1
Ryan <373005226@qq.com>
2020-08-14 18:35:18 +0800

    修改title名字

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,13 +1,13 @@
 module.exports = {
   themeConfig: {
-    logo: '/assets/img/favicon.png',
     nav: [
       { text: '首页', link: '/' },
       { text: '我的博客', link: 'https://www.ly-blog.top/' },
-      { text: '日常积累', link: '/accumulation/' },
-      { text: '面试题积累', link: '/Interview/' },
+      { text: '学习笔记', link: '/accumulation/' },
+      { text: '面试宝典', link: '/Interview/' },
       { text: '算法积累', link: '/algorithm/' },
-      { text: '微信公众号文章积累', link: '/wechat_article/' },
+      { text: '优文转载', link: '/article/' },
+      { text: '工具箱', link: '/tool/' },
       { text: '关于我', link: '/about/' },
     ],
     sidebar: 'auto'
--- docs/README.md
+++ docs/README.md
@@ -1,16 +1,16 @@
 ---
 home: true
-# heroImage: /hero.png
+heroImage: null
 heroText: Ryan的网站
-tagline: null
+tagline: 个人知识点系统整理
 actionText: 点击跳转个人博客 →
 actionLink: https://www.ly-blog.top
 features:
-- title: 知识积累
-  details: 积累每天所学的难点和心得
-- title: 面试题积累
+- title: 学习笔记
+  details: 积累每天所学的知识
+- title: 面试宝典
   details: 时常关注于互联网各大公司的面试题，以此来锻炼自己的思维，并且写下所悟所得
-- title: 公众号知识积累
-  details: 观察到的好的公众号，会对文章进行分类来进行存储，以便与记忆
-footer: MIT Licensed | Copyright © 2018-present Evan You
+- title: 优文转载
+  details: 会将优秀的公众号进行分类
+footer: MIT Licensed | Copyright © 2020-present Ryan
 ---
