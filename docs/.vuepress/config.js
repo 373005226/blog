bbba542bb879c0321e65dbe3642ce2c5fafb92aa
kitty <kitty@eternityx.com>
2020-08-14 16:41:52 +0800

    第一次提交

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  themeConfig: {
+    logo: '/assets/img/favicon.png',
+    nav: [
+      { text: '首页', link: '/' },
+      { text: '我的博客', link: 'https://www.ly-blog.top/' },
+      { text: '关于我', link: '/about/' },
+    ],
+    sidebar: 'auto'
+  }
+}
