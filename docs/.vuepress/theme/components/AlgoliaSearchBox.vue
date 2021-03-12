c0810bc745de4269670663e29730112ee962b234
liuyuan <373005226@qq.com>
2021-03-12 15:19:32 +0800

    debug 打包错误

--- docs/.vuepress/theme/components/AlgoliaSearchBox.vue
+++ docs/.vuepress/theme/components/AlgoliaSearchBox.vue
@@ -1,72 +1,73 @@
 <template>
-  <form
-    id="search-form"
-    class="algolia-search-wrapper search-box"
-    role="search"
-  >
-    <i class="iconfont reco-search"></i>
-    <input
-      id="algolia-search-input"
-      class="search-query"
-      :placeholder="placeholder"
-    >
-  </form>
+    <div>
+        <form id="search-form" class="algolia-search-wrapper search-box" role="search">
+            <i class="iconfont reco-search"></i>
+            <input id="algolia-search-input" class="search-query" :placeholder="placeholder" />
+        </form>
+    </div>
 </template>
 
 <script>
 export default {
-  props: ['options'],
-  data () {
-    return {
-      placeholder: undefined
-    }
-  },
-  mounted () {
-    this.initialize(this.options, this.$lang)
-    this.placeholder = this.$site.themeConfig.searchPlaceholder || ''
-  },
-
-  methods: {
-    initialize (userOptions, lang) {
-      Promise.all([
-        import(/* webpackChunkName: "docsearch" */ 'docsearch.js/dist/cdn/docsearch.min.js'),
-        import(/* webpackChunkName: "docsearch" */ 'docsearch.js/dist/cdn/docsearch.min.css')
-      ]).then(([docsearch]) => {
-        docsearch = docsearch.default
-        const { algoliaOptions = {} } = userOptions
-        docsearch(Object.assign(
-          {},
-          userOptions,
-          {
-            inputSelector: '#algolia-search-input',
-            // #697 Make docsearch work well at i18n mode.
-            algoliaOptions: Object.assign({
-              'facetFilters': [`lang:${lang}`].concat(algoliaOptions.facetFilters || [])
-            }, algoliaOptions),
-            handleSelected: (input, event, suggestion) => {
-              const { pathname, hash } = new URL(suggestion.url)
-              this.$router.push(`${pathname}${hash}`)
-            }
-          }
-        ))
-      })
+    props: ['options'],
+    data() {
+        return {
+            placeholder: undefined
+        }
+    },
+    mounted() {
+        this.initialize(this.options, this.$lang)
+        this.placeholder = this.$site.themeConfig.searchPlaceholder || ''
     },
 
-    update (options, lang) {
-      this.$el.innerHTML = '<input id="algolia-search-input" class="search-query">'
-      this.initialize(options, lang)
-    }
-  },
+    methods: {
+        initialize(userOptions, lang) {
+            Promise.all([
+                import(
+                    /* webpackChunkName: "docsearch" */ 'docsearch.js/dist/cdn/docsearch.min.js'
+                ),
+                import(
+                    /* webpackChunkName: "docsearch" */ 'docsearch.js/dist/cdn/docsearch.min.css'
+                )
+            ]).then(([docsearch]) => {
+                docsearch = docsearch.default
+                const { algoliaOptions = {} } = userOptions
+                docsearch(
+                    Object.assign({}, userOptions, {
+                        inputSelector: '#algolia-search-input',
+                        // #697 Make docsearch work well at i18n mode.
+                        algoliaOptions: Object.assign(
+                            {
+                                facetFilters: [`lang:${lang}`].concat(
+                                    algoliaOptions.facetFilters || []
+                                )
+                            },
+                            algoliaOptions
+                        ),
+                        handleSelected: (input, event, suggestion) => {
+                            const { pathname, hash } = new URL(suggestion.url)
+                            this.$router.push(`${pathname}${hash}`)
+                        }
+                    })
+                )
+            })
+        },
 
-  watch: {
-    $lang (newValue) {
-      this.update(this.options, newValue)
+        update(options, lang) {
+            this.$el.innerHTML = '<input id="algolia-search-input" class="search-query">'
+            this.initialize(options, lang)
+        }
     },
 
-    options (newValue) {
-      this.update(newValue, this.$lang)
+    watch: {
+        $lang(newValue) {
+            this.update(this.options, newValue)
+        },
+
+        options(newValue) {
+            this.update(newValue, this.$lang)
+        }
     }
-  }
 }
 </script>
 
@@ -166,5 +167,4 @@ export default {
       width 5px
       margin -3px 3px 0
       vertical-align middle
-
 </style>
